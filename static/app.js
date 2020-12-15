b42ecc0cbf2c5308136a7eba8adc7e279926385a
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-12-14 16:00:22 -0800

    Front End

--- static/app.js
+++ static/app.js
@@ -1,32 +1,52 @@
-const BASE_URL = "/api/cupcakes";
-$cupcakeList = $("#cupcake-list");
+const CUPCAKES_ENDPOINT = "/api/cupcakes";
+const $cupcakeList = $("#cupcake-list");
+const $cupcakeForm = $("#cupcake-form");
 
 /* GET request to display all the cupcakes */
-async function fetchCupcakeList(){
-  const response = await axios.get(BASE_URL);
+async function fetchCupcakeList() {
+  const response = await axios.get(CUPCAKES_ENDPOINT);
   let cupcakeList = response.data.cupcakes;
 
   console.log(response.data.cupcakes)
 
-  for(let cupcake of cupcakeList){
+  for (let cupcake of cupcakeList) {
     generateMarkupAndAppend(cupcake);
   }
 }
 
 /* Genereate markup for each cupcake and append it to index.html */
-function generateMarkupAndAppend(cupcake){
-  let html = `<img class="img-thumbnail w-25" src="${cupcake.image}">
-              <li>${cupcake.flavor}, ${cupcake.size}, 
-              ${cupcake.rating}</li>`;
+function generateMarkupAndAppend(cupcake) {
+  let html = `<li class="list-group">
+                <div class="list-group-item">
+                  <h5 class=mb-1">Flavor: ${cupcake.flavor}</h5>
+                  <img class="img-thumbnail list-group-item lw-25" src="${cupcake.image}">
+                  <p class="mb-1">Size: ${cupcake.size}, Rating: ${cupcake.rating}</p>
+                </div>
+              </li>`;
 
   $cupcakeList.append(html);
 }
 
+/* Grabs form data then sends AJAX request to
+ * POST /api/cupcakes
+ * which adds a cupcake
+ */
 
-/* After page loads, fetch the cupcakes list */
-// $( document ).ready(function(){
-//   fetchCupcakeList();
-// });
-
+async function handleFormData(evt){
+  evt.preventDefault();
+  // GET FORM DATA
+  let flavor = $("#flavor").val();
+  let size = $("#size").val();
+  let rating =$("#rating").val();
+  let image = $("#image").val();
+  let cupcakeFormData = {"flavor":flavor, "size":size, "rating":rating, "image":image};
+  console.log(cupcakeFormData);
+  // Return the request reponse
+  let cupcake = await axios.post(CUPCAKES_ENDPOINT, cupcakeFormData);
+  console.log(cupcake.data.cupcake);
+  generateMarkupAndAppend(cupcake.data.cupcake);
+}
 
 fetchCupcakeList();
+
+$cupcakeForm.on("submit", handleFormData)
